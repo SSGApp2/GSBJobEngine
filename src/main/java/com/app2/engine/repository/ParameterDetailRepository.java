213bca454e2b6e71c9e2abfe5639eb113806e2fa
pugun <pugun0213@gmail.com>
2020-07-20 18:10:59 +0700

    masterData

--- src/main/java/com/app2/engine/repository/ParameterDetailRepository.java
+++ src/main/java/com/app2/engine/repository/ParameterDetailRepository.java
@@ -1,5 +1,6 @@
 package com.app2.engine.repository;
 
+import com.app2.engine.entity.app.Parameter;
 import com.app2.engine.entity.app.ParameterDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ParameterDetailRepository extends JpaRepository<ParameterDetail, Long> {
+    List<ParameterDetail> findByParameter(@Param("parameter") Parameter parameter);
     @Query("select o from ParameterDetail o join o.parameter b where b.code=:pCode and o.code=:dCode ")
     ParameterDetail findByParameterAndCode(@Param("pCode") String pCode, @Param("dCode") String dCode);
 
--- src/main/java/com/app2/engine/service/SmbFileService.java
+++ src/main/java/com/app2/engine/service/SmbFileService.java
@@ -3,4 +3,5 @@ package com.app2.engine.service;
 public interface SmbFileService {
     public String copyRemoteFileToLocalFile(String remotePath);
     public String copyLocalFileToRemoteFile(String remotePath);
+    public String copyRemoteFolderToLocalFolder(String path);
 }
