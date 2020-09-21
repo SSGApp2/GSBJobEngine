10dd15729aeef03f364d8b0dacc67876e45dc898
DevNoy <thanakorn.noy@gmail.com>
2020-09-21 16:36:46 +0700

    Fix type file to lowercase

--- src/main/java/com/app2/engine/service/impl/WRNServiceImpl.java
+++ src/main/java/com/app2/engine/service/impl/WRNServiceImpl.java
@@ -30,8 +30,8 @@ public class WRNServiceImpl extends AbstractEngineService implements WRNService 
         try {
             // --- Copy File WRN_CONSENT_YYYYMMDD.txt ---
             String curDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
-            String fileName = "WRN_CONSENT_" + curDate + ".TXT";
-//            String pathName = "D:/ProjectInSoft/GSB/WRN_CONSENT_20200714.TXT";
+            String fileName = "WRN_CONSENT_" + curDate + ".txt";
+//            String pathName = "D:/ProjectInSoft/GSB/WRN_CONSENT_20200714.txt";
             String pathName = smbFileService.remoteFileToLocalFile(fileName,"WRN");
             // --- Send To GSBEngine Type 1 ---
             ResponseEntity<String> result = getResultByExchange(API_WRN + "?type=1&pathFile=" + pathName);
@@ -50,8 +50,8 @@ public class WRNServiceImpl extends AbstractEngineService implements WRNService 
         try {
             // --- Copy File WRN_TDR_YYYYMMDD.txt ---
             String curDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
-            String fileName = "WRN_TDR_" + curDate + ".TXT";
-//            String pathName = "D:/ProjectInSoft/GSB/WRN_TDR_20200714.TXT";
+            String fileName = "WRN_TDR_" + curDate + ".txt";
+//            String pathName = "D:/ProjectInSoft/GSB/WRN_TDR_20200714.txt";
             String pathName = smbFileService.remoteFileToLocalFile(fileName,"WRN");
             // --- Send To GSBEngine Type 2 ---
             ResponseEntity<String> result = getResultByExchange(API_WRN + "?type=2&pathFile=" + pathName);
